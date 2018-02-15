e69551696ea51da2c2cecf6701709df4cde8df68
Kirill <opoxmeloid@yandex.ru>
2018-02-15 13:26:41 +0300

    Style changes

--- src/components/editor-component.js
+++ src/components/editor-component.js
@@ -1,6 +1,31 @@
 import CKEditor from 'react-ckeditor-component'
 import React from 'react'
 import request from 'request-promise-native'
+import { css } from 'emotion'
+import { Button, Grid, Row, Col } from 'react-bootstrap'
+
+const editorBlock = css`
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 10px;
+  padding-right: 10px;
+  max-width: 1680px;
+`
+
+const editorSection = css`
+  margin-bottom: 1rem;
+  
+  border: 1px solid rgba(0,0,0,.15);
+  display: inline-block;
+`
+
+const buttonStyle = css`
+  font-size: x-large;
+`
+
+const sectionTitle = css`
+  position: absolute;
+`
 
 export default class Editor extends React.Component {
   constructor (props) {
@@ -10,7 +35,7 @@ export default class Editor extends React.Component {
     this.onChangeHeader = this.onChangeHeader.bind(this)
     this.onButtonClick = this.onButtonClick.bind(this)
     this.state = {
-      content: 'content',
+      content: 'Document content',
       header: ''
     }
   }
@@ -61,24 +86,54 @@ export default class Editor extends React.Component {
 
   render () {
     return (
-      <div>
-        <h2>Header</h2>
-        <CKEditor
-          activeClass='cke4-inline__anchor cke_editable cke_editable_inline cke_contents_ltr cke_show_borders'
-          content={'Edit header here'}
-          events={{
-            'change': this.onChangeHeader
-          }}
-        />
-        <h2>Main section</h2>
-        <CKEditor
-          content={this.state.content}
-          events={{
-            'change': this.onChange
-          }}
-        />
-        <button onClick={this.onButtonClick}>Save</button>
-      </div>
+      <Grid className={editorBlock}>
+        <Row>
+          <Col md={2} sm={12}>
+            <h3>Header</h3>
+          </Col>
+          <Col md={8} sm={12}>
+            <CKEditor
+              activeClass={editorSection}
+              content={'Edit header here'}
+              events={{
+                'change': this.onChangeHeader
+              }}
+              config={{
+                docType: '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">',
+                defaultLanguage: 'ru',
+                // extraPlugins: 'autogrow',
+                autoGrow_minHeight: 200,
+                autoGrow_maxHeight: 600,
+                autoGrow_bottomSpace: 50
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={2} sm={12}>
+            <h3>Main section</h3>
+          </Col>
+          <Col md={8} sm={12}>
+            <CKEditor
+              activeClass={editorSection}
+              content={this.state.content}
+              events={{
+                'change': this.onChange
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              onClick={this.onButtonClick}
+              className={buttonStyle}
+            >
+              Dowload DOCX
+            </Button>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
