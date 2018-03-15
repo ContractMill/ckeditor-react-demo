0b27053a473360fed12bbd426571602104d72d49
Кирилл <kirill@macbook.local>
2018-03-15 15:35:15 +0300

    Headers quickfix

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -223,7 +223,7 @@ export default class Editor extends React.Component {
                 enterMode: 2, // CKEDITOR.ENTER_BR,
                 shiftEnterMode: 1, // CKEDITOR.ENTER_P,
                 line_height: '1.0;1.5;2.0;2.5;3.0',
-                allowedContent: 'div{*}',
+                // allowedContent: 'div{*}',
                 fullPage: true
               }}
             />
